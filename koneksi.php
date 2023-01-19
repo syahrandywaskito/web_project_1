f31cb6bbc5c20e1136325dd37df2e787e6eeb9b4
syahrandywaskito <syahrandywaskito666@gmail.com>
2023-01-19 17:11:21 +0700

    tambahan file koneksi.php

---
+++ koneksi.php
@@ -0,0 +1,11 @@
+<?php
+
+// database
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "pemesanan_kamarHotel";
+
+$koneksi = new mysqli("$servername", "$username", "$password", "$database")
+
+?>
--- login.php
+++ login.php
@@ -45,6 +45,28 @@
         <!--  -->
 
         <!-- form login -->
+        <section>
+          <div class="container" style="height: 70vh">
+            <div class=" d-flex align-items-center justify-content-center" style="height: 80vh">
+              <div class="card col-md-4" style="width: 25rem;">
+                <div class="card-body">
+                  <h5 class="card-title">LOGIN</h5>
+                  <form action="proses_login.php" method="post" class="pt-2">
+                    <div class="mb-3">
+                      <label for="username" class="form-label">Username</label>
+                      <input type="text" class="form-control" name="username" id="username">
+                    </div>
+                    <div class="mb-3">
+                      <label for="password" class="form-label">Password</label>
+                      <input type="password" class="form-control" name="password" id="password">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
         <!--  -->
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
