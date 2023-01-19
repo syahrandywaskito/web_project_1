c3fb6362362c75cb928e67288b0f74738ebe06a1
syahrandywaskito <syahrandywaskito666@gmail.com>
2023-01-19 21:13:16 +0700

    fix login validation

--- proses_login.php
+++ proses_login.php
@@ -6,25 +6,31 @@ session_start();
 include "koneksi.php";
 $username = $_POST['username'];
 $pwd = $_POST['password'];
-$query = "SELECT username, pwd FROM user ORDER BY username";
 
+// syntax SQL untuk mengambil semua data sesuai dengan input dari login
+$query = "SELECT * FROM user WHERE username = '$username' AND pwd = '$pwd' ";
+
+// $result sebagai tempat untuk object dari $koneksi dan $query
 $result = mysqli_query($koneksi, $query);
-$row = mysqli_fetch_assoc($result);
 
-if (($username == $row['username']) AND ($pwd == $row['pwd'])) {
-    $_SESSION['username'] = $row['username'];
-    $_SESSION['pwd'] = $row['pwd'];
-    echo "selamat anda berhasil login";
+if ($result->num_rows > 0) {
+    // $row sebagai wadah untuk fetch dari database dengan result sebagai object dari database
+    $row = mysqli_fetch_assoc($result);
+        $_SESSION['username'] = $row['username'];
+        $_SESSION['pwd'] = $row['pwd'];
+        // echo "selamat anda berhasil login";
+        var_dump($result);
+
 }
-
+    
 elseif (($username == "admin") AND ($pwd == "toor")) {
-    $_SESSION['username'] = $username;
-    $_SESSION['pwd'] = $pwd;
-    echo "selamat anda berhasil login sebagai admin";
+        $_SESSION['username'] = $username;
+        $_SESSION['pwd'] = $pwd;
+        echo "selamat anda berhasil login sebagai admin";
 }
-
+    
 else {
-    header("location:login.php");
+        header("location:login.php");
 }
 
 ?>
